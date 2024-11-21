667d040399430ca6e76fd53b97f0de9579c59928
nirdahan9 <nir.dahan01@post.runi.ac.il>
2024-11-21 09:04:47 +0200

    21.11 09:04

--- Algebra.java
+++ Algebra.java
@@ -6,62 +6,126 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
+		System.out.println(plus(2,3));   // 2 + 3 = 5
+	    System.out.println(minus(7,2));  // 7 - 2 = 7
+   		System.out.println(minus(2,7));  // 2 - 7 = -5
+ 		System.out.println(times(3,4));  // 3 * 4 = 12
+   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2 = 10
+   		System.out.println(pow(5,3));      // 5^3 = 125
+   		System.out.println(pow(3,5));      // 3^5 = 243
+   		System.out.println(div(12,3));   // 12 / 3 = 4
+   		System.out.println(div(5,5));    // 5 / 5  = 1 
+   		System.out.println(div(25,7));   // 25 / 7 = 3 
+   		System.out.println(mod(25,7));   // 25 % 7 = 4
+   		System.out.println(mod(120,6));  // 120 % 6 = 0  
+   		System.out.println(sqrt(36)); 	   // 6
+		System.out.println(sqrt(263169));    // 513
+   		System.out.println(sqrt(76123));     // 275
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if(x2<0) {
+			int x2Converted = x2;
+			for(int i = 0 ; i > x2 ; i --) {
+				x2Converted ++;
+				x2Converted ++;
+			}
+			x2 = x2Converted;
+			for(int i = 0 ; i < x2 ; i ++) {
+				x1 --;
+			}
+		}
+		else {
+			for(int i = 0 ; i < x2 ; i ++) {
+				x1 ++;
+			}
+		} 
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if(x2<0) {
+			int x2Converted = x2;
+			for(int i = 0 ; i > x2 ; i --) {
+				x2Converted ++;
+				x2Converted ++;
+			}
+			x2 = x2Converted;
+			for(int i = 0 ; i < x2 ; i ++) {
+				x1 ++;
+			}
+		}
+		else {
+			for(int i = 0 ; i < x2 ; i ++) {
+				x1 --;
+			}
+		}
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 0 ;
+		if (x1 == 0 || x2 == 0) return 0;
+		for(int i = 0 ; i < x2 ; i ++) {
+			result = plus(result, x1);
+		}
+		if ((x1 < 0 && x2 > 0) || (x2 < 0 && x1 > 0)) {
+			for( int i = 0 ; i < result ; i ++) {
+				result --;
+				result --;
+			}
+		}
+		return result;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 1;
+		if(x==0) return 0;
+		if(n==1) return 1;
+		for(int i = 0 ; i < n ; i ++) {
+			result = times(result, x);
+		}
+		if(x<0 && mod(n, 2) == 1) {
+			for( int i = 0 ; i < result ; i ++) {
+				result --;
+				result --;
+			}
+		}
+		return result;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int times = 0;
+		while(x1>=x2) {
+			x1 = minus(x1, x2);
+			times ++;
+		}
+		if ((x1 < 0 && x2 > 0) || (x2 < 0 && x1 > 0)) {
+			for( int i = 0 ; i < times ; i ++) {
+				times --;
+				times --;
+			}
+		}
+		return times;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int div = div(x1, x2);
+		int mul = times(div , x2);
+		return x1 - mul;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 0;
+		while(times(result, result) < x) result ++ ;
+		if(times(result , result) != x) result --;
+		return result;
 	}	  	  
 }
